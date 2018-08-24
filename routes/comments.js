1f8fcc8c15c10130942b5d5778300aa1d333053c
memo-saldana <memo.sal98@gmail.com>
2018-08-24 17:00:41 -0500

    Implemented comment authorization middleware

--- routes/comments.js
+++ routes/comments.js
@@ -44,7 +44,7 @@ router.post("/",isLoggedIn, function(req,res) {
 })
 
 // EDIT
-router.get("/:comment_id/edit", function(req,res) {
+router.get("/:comment_id/edit",checkCommentOwnership, function(req,res) {
 	Comment.findById(req.params.comment_id, function(err,foundComment) {
 		if(err){
 			console.log(err);
@@ -58,7 +58,7 @@ router.get("/:comment_id/edit", function(req,res) {
 
 
 // UPDATE
-router.put("/:comment_id", function(req,res) {
+router.put("/:comment_id", checkCommentOwnership, function(req,res) {
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err,updatedComment) {
 		if(err){
 			res.redirect("back");
@@ -70,7 +70,7 @@ router.put("/:comment_id", function(req,res) {
 
 
 // COMMENT DESTROY
-router.delete("/:comment_id", function(req,res) {
+router.delete("/:comment_id", checkCommentOwnership, function(req,res) {
 	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
 		if(err){
 			console.log(err);
@@ -88,5 +88,25 @@ function isLoggedIn(req,res,next) {
 	}
 	res.redirect("/login")
 }
+
+function checkCommentOwnership(req,res,next) {
+		if(req.isAuthenticated()){
+			Comment.findById(req.params.comment_id, function(err,foundComment){
+				if(err){
+					console.log(err);
+					res.redirect("back");
+				} else { //Does user own comment?
+					if(foundComment.author.id.equals(req.user._id)){
+						next();
+					} else {
+						res.redirect("back");
+					}
+				}
+			});
+		} else {
+			res.redirect("back");
+		}	
+}
+
 
 module.exports = router;
