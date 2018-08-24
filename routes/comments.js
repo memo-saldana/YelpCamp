268e6fedbf6554d7c8c4828ab3fc2ec7eb2db7a1
memo-saldana <memo.sal98@gmail.com>
2018-08-24 14:59:32 -0500

    Comment edit and update routes implemented

--- routes/comments.js
+++ routes/comments.js
@@ -45,11 +45,28 @@ router.post("/",isLoggedIn, function(req,res) {
 
 // EDIT
 router.get("/:comment_id/edit", function(req,res) {
-	
-	req.params.comment_id
-	res.render("comments/edit",{campground_id: req.params.id })
-	// res.render("comments/edit");
-})
+	Comment.findById(req.params.comment_id, function(err,foundComment) {
+		if(err){
+			console.log(err);
+			res.redirect("back");
+		} else {
+			
+			res.render("comments/edit",{campground_id: req.params.id, comment: foundComment});
+		}
+	});
+});
+
+
+// UPDATE
+router.put("/:comment_id", function(req,res) {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err,updatedComment) {
+		if(err){
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	})
+});
 
 // middleware
 function isLoggedIn(req,res,next) {
